37c6d040f19f2d3513e53f977d5d5e1d6623bafb
pnaskardev <pnaskardev@gmail.com>
2023-09-17 15:34:48 +0530

    added some validation in the serializers and ValueError taken care of

--- user/models.py
+++ user/models.py
@@ -2,64 +2,6 @@
 import uuid
 from django.db import models
 
-# class UserManager(BaseUserManager):
-#     def create_user(self, adhaar_id,email, name, annual_income, password, **extra_fields):
-#         if not email:
-#             raise ValueError("User must have an email address")
-#         if not name:
-#             raise ValueError("User must have a name")
-#         if not annual_income:
-#             raise ValueError("User must have an annual income")
-#         if not adhaar_id:
-#             raise ValueError("User must have an aadhar id")
-
-#         extra_fields.setdefault('is_staff', False)
-#         extra_fields.setdefault('is_superuser', False)
-#         extra_fields.setdefault('is_active', True)
-
-#         user = self.model(
-#             email=self.normalize_email(email),
-#             name=name,
-#             annual_income=annual_income,
-#             adhaar_id=adhaar_id,
-#             **extra_fields
-#         )
-#         user.set_password(password)
-#         user.save(using=self._db)
-#         return user
-
-#     def create_superuser(self, adhaar_id,email, name, annual_income, **extra_fields):
-        
-#         extra_fields.setdefault('is_staff', True)
-#         extra_fields.setdefault('is_superuser', True)
-#         extra_fields.setdefault('is_active', True)
-#         return self.create_user(
-#             email=self.normalize_email(email),
-#             name=name,
-#             annual_income=annual_income,
-#             adhaar_id=adhaar_id,
-#             **extra_fields
-#         )
-
-# class User(AbstractBaseUser, PermissionsMixin):
-#     adhaar_id = models.UUIDField(
-#         primary_key=True, default=uuid.uuid4, editable=True, unique=True)
-#     email = models.EmailField(verbose_name='email', max_length=60, unique=True)
-#     name = models.CharField(max_length=30)
-#     annual_income = models.IntegerField(
-#         default=0, verbose_name='Annual Income')
-
-#     is_superuser = models.BooleanField(default=False)
-#     is_staff = models.BooleanField(default=False)
-#     is_active = models.BooleanField(default=True)
-#     USERNAME_FIELD = 'email'
-#     REQUIRED_FIELDS = ['adhaar_id','name', 'annual_income', ]
-
-#     objects = UserManager()
-
-#     def __str__(self):
-#         return self.email
-
 class CustomerManager(models.Manager):
     def create(self, **kwargs):
         # Perform custom validation logic here
--- user/serializers.py
+++ user/serializers.py
@@ -6,3 +6,24 @@ class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = '__all__'
+    
+    def validate(self, data):
+        # Add your custom validation logic here
+        email = data.get('email')
+        name = data.get('name')
+        annual_income = data.get('annual_income')
+        adhaar_id = data.get('adhaar_id')
+
+        if not email:
+            raise serializers.ValidationError("User must have an email address")
+        if annual_income is None or annual_income < 0:
+            raise serializers.ValidationError("Annual income must be a non-negative value")
+        if not name:
+            raise serializers.ValidationError("User must have a name")
+        if not adhaar_id:
+            raise serializers.ValidationError("User must have an Aadhar ID")
+
+        return data
+
+    def create(self, validated_data):
+        return super().create(validated_data)
